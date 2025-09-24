4cfe857148fa5dec24867f9114c9918083b12798
Juliet Nwagwu Ume-Ezeoke <jnwagwu@stanford.edu>
2025-09-24 16:07:11 -0700

    prelim studies for alternate arrow construction

--- tests/test_visuals/test_data_plot.py
+++ tests/test_visuals/test_data_plot.py
@@ -1,4 +1,3 @@
-
 import pytest
 from replan2eplus.examples.defaults import PATH_TO_IDD
 from replan2eplus.ezcase.read import ExistCase, get_afn_objects
@@ -6,13 +5,12 @@ from replan2eplus.idfobjects.variables import OutputVariables
 from replan2eplus.paths import TWO_ROOM_RESULTS, TWO_ROOM_AIRBOUNDARY_RESULTS
 from replan2eplus.results.sql import create_result_for_qoi, get_sql_results
 from replan2eplus.visuals.data_plot import DataPlot, filter_data_arr
-from pathlib import Path 
+from pathlib import Path
 
 
 def get_qoi(qoi: OutputVariables, path: Path = TWO_ROOM_RESULTS):
     sql = get_sql_results(path)
     return create_result_for_qoi(sql, qoi)
-
 
 
 def plot_zone_data():
@@ -33,8 +31,12 @@ def plot_zone_data():
 def plot_connection_data():
     # TODO the sql needs to be linked to the case reading!!!!
     case = ExistCase(PATH_TO_IDD, TWO_ROOM_AIRBOUNDARY_RESULTS / "out.idf")
-    flow_12 = get_qoi("AFN Linkage Node 1 to Node 2 Volume Flow Rate", TWO_ROOM_AIRBOUNDARY_RESULTS)
-    flow_21 = get_qoi("AFN Linkage Node 2 to Node 1 Volume Flow Rate", TWO_ROOM_AIRBOUNDARY_RESULTS)
+    flow_12 = get_qoi(
+        "AFN Linkage Node 1 to Node 2 Volume Flow Rate", TWO_ROOM_AIRBOUNDARY_RESULTS
+    )
+    flow_21 = get_qoi(
+        "AFN Linkage Node 2 to Node 1 Volume Flow Rate", TWO_ROOM_AIRBOUNDARY_RESULTS
+    )
     combined_flow = flow_12.select_time(1) - flow_21.select_time(1)
 
     print(combined_flow)
@@ -46,8 +48,29 @@ def plot_connection_data():
     dp.plot_subsurfaces_and_surfaces(case.afn, case.airboundaries, case.subsurfaces)
     dp.plot_connections_with_data(combined_flow, case.subsurfaces, case.airboundaries)
 
-    return dp 
+    return dp
 
+
+def plot_zones_and_connections(
+    path: Path = TWO_ROOM_AIRBOUNDARY_RESULTS, hour: int = 1
+):
+    case = ExistCase(PATH_TO_IDD, path / "out.idf")
+
+    pressure = get_qoi("AFN Node Total Pressure", path)
+    data_at_noon = pressure.select_time(hour)
+
+    flow_12 = get_qoi("AFN Linkage Node 1 to Node 2 Volume Flow Rate", path)
+    flow_21 = get_qoi("AFN Linkage Node 2 to Node 1 Volume Flow Rate", path)
+    combined_flow = flow_12.select_time(hour) - flow_21.select_time(hour)
+
+    dp = DataPlot(case.zones)
+    dp.plot_zones_with_data(data_at_noon)
+    dp.plot_zone_names()
+    dp.plot_cardinal_names()
+    dp.plot_subsurfaces_and_surfaces(case.afn, case.airboundaries, case.subsurfaces)
+    dp.plot_connections_with_data(combined_flow, case.subsurfaces, case.airboundaries)
+
+    return dp 
 
 
 @pytest.mark.xfail()
@@ -64,7 +87,13 @@ def test_plot_zones():
     plot_zone_data()
     assert 1
 
-# MOVE ELSEWHERE -> test of existing, ot dataplot.. 
+
+def test_plot_connections():
+    plot_connection_data()
+    assert 1
+
+
+# MOVE ELSEWHERE -> test of existing, ot dataplot..
 def test_get_afn_values():
     case = ExistCase(PATH_TO_IDD, TWO_ROOM_AIRBOUNDARY_RESULTS / "out.idf")
     assert len(case.afn.zones) == 2
@@ -72,9 +101,8 @@ def test_get_afn_values():
     assert len(case.afn.subsurfaces) == 3
 
 
-
 if __name__ == "__main__":
-    dp = plot_connection_data()
+    dp = plot_zones_and_connections(TWO_ROOM_RESULTS)
     dp.show()
     # get_afn_values()
     pass
