671e6167d5ceed9a4f3598f521a556860cf6b5d9
Juliet Nwagwu Ume-Ezeoke <jnwagwu@stanford.edu>
2025-10-31 18:39:39 -0700

    need to test logic for creatinf ventilation schedules

--- src/replan2eplus/ops/schedules/create.py
+++ src/replan2eplus/ops/schedules/create.py
@@ -6,14 +6,13 @@ from geomeppy import IDF
 # from replan2eplus.ops.afn.utils import ScheduleInput
 
 
-def create_schedule(idf: IDF, schedule_inputs: ScheduleInput, folder_path: Path):
-    name, type_limits, year = schedule_inputs
-    file_path = folder_path / name 
-    idf_sched = IDFScheduleFile(name, type_limits, Path(file_path))
-    year.write(folder_path)
-    idf_sched.write(idf)
+def create_schedule(idf: IDF, schedule_inputs: list[ScheduleInput], folder_path: Path):
+    def create(schedule: ScheduleInput):
+        name, type_limits, year = schedule
+        file_path = folder_path / name
+        idf_sched = IDFScheduleFile(name, type_limits, Path(file_path))
+        year.write_to_file(folder_path)
+        idf_sched.write(idf)
 
-# def save_schedules(idf: IDF, folder_path: Path):
-#     idf_schedules = IDFScheduleFile().read(idf)
-#     for idf_sched in idf_schedules:
-#         idf_sched.update_file_name(idf, folder_path)
+    for sched in schedule_inputs:
+        create(sched)
